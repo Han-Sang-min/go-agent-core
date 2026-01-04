4a71d4bfb27e730e5853881ecb270cd186d0abc6
Han_Sang_Min <ghtrm1037@gmail.com>
2026-01-04 06:16:39 +0000

    Patch at STEP 03

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -15,6 +15,7 @@ import (
 var (
 	loc = mustLoadLocation("Asia/Seoul")
 )
+
 var counter atomic.Int64
 
 func mustLoadLocation(src string) *time.Location {
@@ -29,6 +30,15 @@ func main() {
 	var env collector.RuntimeEnv = collector.DetectEnv()
 	fmt.Printf("Detected Environment: %s\n", env.Kind())
 
+	config := flag.String("config", "", "config file path")
+	once := flag.Bool("once", false, "run once")
+	flag.Parse()
+
+	_ = config
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
 	defer signal.Stop(sigCh)
@@ -36,19 +46,10 @@ func main() {
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
 
-	config := flag.String("config", "", "config file path")
-	once := flag.Bool("once", false, "run once")
-
-	flag.Parse()
-
-	_ = config
-	_ = once
-
 	fmt.Print("Agent Start.\n")
 
 	collect := func() {
-		ctx := context.Background()
-
+		seq := counter.Add(1)
 		cpuInfo, err := env.CPU(ctx)
 		if err != nil {
 			fmt.Printf("CPU Error: %v\n", err)
@@ -59,10 +60,21 @@ func main() {
 			fmt.Printf("Mem Error: %v\n", err)
 		}
 
-		fmt.Printf("[Time: %s] CPU: %.2f%%, Mem: %.2f%%\n",
-			time.Now().Format(time.RFC3339),
+		diskInfo, err := env.Disk(ctx)
+		if err != nil {
+			fmt.Printf("Disk Error: %v\n", err)
+		}
+
+		procInfo, err := env.Procs(ctx)
+		if err != nil {
+			fmt.Printf("Proc Error: %v\n", err)
+		}
+
+		fmt.Printf("[Seq: %d] [Time: %s] CPU: %.2f%%, Mem: %.2f%%, Disk: %.2f%%, Procs: %d\n",
+			seq,
+			time.Now().In(loc),
 			cpuInfo.UsagePercent,
-			memInfo.UsedPercent)
+			memInfo.UsedPercent, diskInfo.UsedPercent, procInfo.Count)
 	}
 
 	if *once {
